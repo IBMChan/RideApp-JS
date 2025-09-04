69619af1da0be3e4bb4e6b40bcf54572a7f4a205
Chandana G <chandana.g@ibm.com>
2025-09-04 16:45:49 +0530

    functionality

--- src/services/driver-service.js
+++ src/services/driver-service.js
@@ -1,4 +1,4 @@
-//accept a ride -- only if previous rides are completed
+//accept a ride -- only if previous rides are completed and also if vehicle is registered
 //change the ride status --(requested -> accepted -> ongoing -> completed / cancelled only when status is accepted)
 //vehicle registration (only one per driver)
 //vehicle details updation 
